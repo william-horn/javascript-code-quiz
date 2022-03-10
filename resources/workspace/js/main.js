af0a4aebe92c5c31f27f9e9e38f5d007344f04b8
William Horn <williamjosephhorn@gmail.com>
2022-03-10 10:43:25 -0500

    added main dedicated functions to main.js

--- resources/workspace/js/main.js
+++ resources/workspace/js/main.js
@@ -120,3 +120,109 @@ quiz.addQuestion(
         .setRightAnswer("===")
         .setChoices("==", "*=", "!=")
 );
+
+
+/* ------------------- */
+/* Dedicated Functions */
+/* ------------------- */
+
+// update timer element to current time
+// this function can also be used as a callback to the 'startTimer()` method of 'quiz'
+function updateQuizTimer(time) {
+    $timerDisplay.textContent = "Time Left: " + time;
+}
+
+// switch between current and previous screens
+function switchScreen($newScreen) {
+    if ($newScreen === $currentWindow) return;
+    eutil.hideElement($currentWindow);
+    $prevWindow = $currentWindow;
+    eutil.showElement($newScreen);
+    $currentWindow = $newScreen;
+
+    // if the quiz has started 
+    if (quiz.currentQuestionIndex > 0) {
+        eutil.showElement($main);
+    } else {
+        eutil.hideElement($main, true);
+    }
+}
+
+// handle procedurally generated questions and display them on screen
+function generateNextQuestion() {
+    eutil.clearChildrenOnElement($answerButtonContainer); // remove old answer buttons
+    eutil.hideElement($questionFeedback);       // hide question feedback footer
+
+    const question = quiz.getNextQuestion();    // get next question from quiz builder
+    if (!question) return;                      // if no next question exists, exit function with null
+
+    // randomize answer choices inside of question object
+    const answerChoices = question.getRandomChoices();
+
+    // get question title from HTML and update it's content
+    const $questionTitle = $questionScreen.querySelector("h2");
+    const $questionFeedbackTitle = $questionFeedback.querySelector("p");
+
+    // generate question title subtexts
+    eutil.generateDynamicText($questionTitle, [
+        {content: "Q." + quiz.currentQuestionIndex + ") "},
+        {content: question.title}
+    ]);
+
+    // !! Generate answer buttons !!
+    answerChoices.forEach((choice) => {
+        // create <li> and <button> elements
+        const $li = document.createElement("li");
+        const $button = document.createElement("button");
+
+        // set the answer button text to the given choice
+        $button.textContent = choice;
+
+        // parent the <button> and <li> elements
+        $li.appendChild($button);
+        $answerButtonContainer.appendChild($li);
+
+        // lock the button heights
+        $li.style.minHeight = $li.clientHeight + "px";
+        $button.style.minHeight = $li.style.minHeight;
+
+        // store a reference to the <button> and corresponding choice text inside the question object
+        question.setButtonForChoice($button, choice)
+    });
+
+    // !! Question answered event !!
+    question.onAnswerStateChanged.connect("onAnswer", (input, state) => {
+        gutil.debugPrint("answer selected (answer state: " + state + ") answer:", input);
+        question.onAnswerStateChanged.disconnect("onAnswer");
+
+        let correctColor = "#88dc8a";
+        let incorrectColor = "#bb2f2f";
+
+        for (let [button, choice] of question.buttons) {
+            if (choice === input) {
+                button.parentElement.style.width = "60%"; // lock width of selected answer at 60%
+
+                // handle when answer is correct
+                if (state === "correct") {
+                    $questionFeedbackTitle.style.color = correctColor;
+                    $questionFeedbackTitle.textContent = "Correct!";
+                } else {
+                    // handle when answer is wrong
+                    button.style.backgroundColor = incorrectColor;
+                    $questionFeedbackTitle.style.color = incorrectColor;
+                    $questionFeedbackTitle.textContent = "Incorrect";
+                    quiz.subtractTime(5);
+                    updateQuizTimer(quiz.getTimeLeft());
+                }
+            }
+
+            // highlight correct answer
+            if (choice === question.rightAnswer) {
+                button.style.backgroundColor = "#39912b";
+            }
+        }
+
+        eutil.showElement($questionFeedback);
+        setTimeout(generateNextQuestion, 2000);
+    });
+}
